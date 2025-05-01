44306c8fe6413d54aa37b277c75ba8a819bbe7bd
son <son7571@github.com>
2025-05-01 10:58:18 +0900

    주소 수정중

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.blog._core.util.Resp;
 
 import java.util.Map;
@@ -21,12 +18,7 @@ public class UserController {
     private final UserService userService;
     private final HttpSession session;
 
-    // ViewResolver -> prefix = /templates/ -> suffix = .mustache
-    @GetMapping("/user/update-form")
-    public String updateForm() {
-        return "user/update-form";
-    }
-
+    // TODO: JWT 이후에
     @PostMapping("/user/update")
     public String update(@Valid UserRequest.UpdateDTO updateDTO, Errors errors) {
         User sessionUser = (User) session.getAttribute("sessionUser");
@@ -43,22 +35,14 @@ public class UserController {
         return Resp.ok(dto);
     }
 
-    @GetMapping("/join-form")
-    public String joinForm() {
-        return "user/join-form";
-    }
 
     @PostMapping("/join")
-    public String join(@Valid UserRequest.JoinDTO reqDTO, Errors errors) {
+    public @ResponseBody Resp<?> join(@Valid @RequestBody UserRequest.JoinDTO reqDTO, Errors errors) {
         UserResponse.DTO respDTO = userService.회원가입(reqDTO);
-        return "redirect:/login-form";
+        return Resp.ok(respDTO);
     }
 
-    @GetMapping("/login-form")
-    public String loginForm() {
-        return "user/login-form";
-    }
-
+    // TODO: JWT 이후에
     @PostMapping("/login")
     public String login(@Valid UserRequest.LoginDTO loginDTO, Errors errors, HttpServletResponse response) {
         //System.out.println(loginDTO);
@@ -78,6 +62,7 @@ public class UserController {
         return "redirect:/";
     }
 
+    // TODO: JWT 이후에
     @GetMapping("/logout")
     public String logout() {
         session.invalidate();
